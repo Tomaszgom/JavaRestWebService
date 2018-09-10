973a17bacb64e4263f1728bf5ed2c83b6cec923c
Tomo <t.gomoradzki@gmail.com>
2018-09-11 01:03:09 +0200

    11/09/2018

---
+++ README.md
@@ -0,0 +1,16 @@
+# Java RESTful Web Service Demo by Tomasz Gomoradzki
+
+Java RESTful Web Service Demo Demo is Java and Jersey based service available on to be deployed on Apache Tomcat.
+
+Once requested it responds with produced list of purchase orders in XML or JSON format, depending on the request.
+
+Web Service has been developed with Eclipse Java EE IDE Oxygen v. 4.7 and Java 8. Works on application server once implemented and launched.
+
+Technologies used and some of the concepts deployed:
+
+- Java 8
+- Apache Tomcat 8.0 Application Server
+- Jersey Library JAX-RS 2.1
+- Annotations
+- Methods: GET, POST
+- Exchange format XML or JSON (both media type are included, the response format depends on the request)
---
+++ src/webService/porders.java
@@ -0,0 +1,144 @@
+package webService;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/ordersService")
+public class porders {
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	@Path("/porders-list")
+	public String pordersListXML(){
+		
+		String response="<?xml version='1.0'?>"
+				+"<porders>"
+					+"<message>"
+					+"<msg>This is response from Java REST Web Service. List of purchase orders in XML format.</msg>"
+					+"<result>successful</result>"
+					+"</message>"	
+					+"<order>"
+						+"<ID>001</ID>"
+						+"<value>120</value>"
+						+"<product>Product01</product>"
+					+"</order>"					
+					+"<order>"
+						+"<ID>002</ID>"
+						+"<value>220</value>"
+						+"<product>Product02</product>"
+					+"</order>"
+					+"<order>"
+						+"<ID>003</ID>"
+						+"<value>160</value>"
+						+"<product>Product03</product>"
+					+"</order>"
+				+"</porders>";
+		
+		return response;
+		
+	}
+	
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/porders-list-json")
+	public String pordersListJSON(){
+		
+		String response="<?xml version='1.0'?>"
+				+"<porders>"
+					+"<message>"
+					+"<msg>This is response from Java REST Web Service. List of purchase orders in JSON format.</msg>"
+					+"<result>successful</result>"
+					+"</message>"	
+					+"<order>"
+						+"<ID>001</ID>"
+						+"<value>120</value>"
+						+"<product>Product01</product>"
+					+"</order>"					
+					+"<order>"
+						+"<ID>002</ID>"
+						+"<value>220</value>"
+						+"<product>Product02</product>"
+					+"</order>"
+					+"<order>"
+						+"<ID>003</ID>"
+						+"<value>160</value>"
+						+"<product>Product03</product>"
+					+"</order>"
+				+"</porders>";
+
+		return response;
+	}
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	@Path("/{id}")
+	public String getOrderXML(@PathParam("id") String id){
+
+		String response;
+		switch(id) {
+			case "001":
+					response="<?xml version='1.0'?>"
+						+"<porders>"
+							+"<message>"
+							+"<msg>Response from Java REST Web Service</msg>"
+							+"<result>Successful</result>"
+							+"</message>"	
+							+"<order>"
+								+"<ID>001</ID>"
+								+"<value>120</value>"
+								+"<product>Product01</product>"
+							+"</order>"
+						+"</porders>";
+				break;
+				
+			case "002":
+					response="<?xml version='1.0'?>"
+						+"<porders>"
+							+"<message>"
+							+"<msg>Response from Java REST Web Service</msg>"
+							+"<result>Successful</result>"
+							+"</message>"	
+							+"<order>"
+								+"<ID>002</ID>"
+								+"<value>220</value>"
+								+"<product>Product02</product>"
+							+"</order>"
+						+"</porders>";
+				break;
+			case "003":
+					response="<?xml version='1.0'?>"
+							+"<porders>"
+								+"<message>"
+								+"<msg>Response from Java REST Web Service</msg>"
+								+"<result>Successful</result>"
+								+"</message>"	
+								+"<order>"
+									+"<ID>003</ID>"
+									+"<value>160</value>"
+									+"<product>Product03</product>"
+								+"</order>"
+							+"</porders>";
+					break;
+
+			default:
+			 		response="<?xml version='1.0'?>"
+					+"<porders>"
+						+"<message>"
+						+"<msg>Response from Java REST Web Service: No such order ID.</msg>"
+						+"<result>Unsuccessful</result>"
+						+"</message>"	
+					+"</porders>";
+			break;
+			
+		}
+		
+		return response;	
+	}
+
+}
