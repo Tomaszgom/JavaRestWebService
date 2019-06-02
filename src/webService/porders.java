6a8ebdeff491918d2931c845198f3ebada1f3044
Tomo <t.gomoradzki@gmail.com>
2019-06-02 18:44:52 +0200

    Refactoring

--- src/webService/porders.java
+++ src/webService/porders.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.MediaType;
  * 
  * @author Admin
  * Purchase Orders RESTful Web Serice Demo
- *
+ * Responds with single or list of Purchase Orders
  */
 
 @Path("/ordersService")
